8d17b9b0ad5efe5a85f06ee69044187da34f5fdc
Mislah <git@mislah.com>
2023-11-03 09:35:00 +0530

    First test

--- Sign-Lang-git/img_classifier/lib/home.dart
+++ Sign-Lang-git/img_classifier/lib/home.dart
@@ -2,7 +2,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tflite/tflite.dart';
-import 'package:image/image.dart' as image;
+import 'package:image/image.dart' as IMG;
 import 'dart:typed_data';
 
 import 'main.dart';
@@ -13,62 +13,25 @@ void loadDetectionModel() async {
     await Tflite.loadModel(
         model: "assets/model.tflite", labels: "assets/labels.txt");
     print("Loaded model successfully");
-    TestModel();
   } on Exception catch (e) {
     print("Failed to load model: " + e.toString());
   }
 }
 
-Future<image.Image> addAndResize(String s) async {
-  var img = Image.asset(
-    s,
-    width: 64,
-    height: 64,
-  );
-  ByteData testImg = (await rootBundle.load(s));
-  image.Image? baseSizeImage = image.decodeImage(testImg.buffer.asUint8List());
-  return image.copyResize(baseSizeImage!, height: 64, width: 64);
-}
-
 Uint8List imageToByteListFloat32(
-    image.Image img, int inputSize, double mean, double std) {
+    IMG.Image img, int inputSize, double mean, double std) {
   var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
   var buffer = Float32List.view(convertedBytes.buffer);
   int pixelIndex = 0;
   for (var i = 0; i < inputSize; i++) {
     for (var j = 0; j < inputSize; j++) {
       var pixel = img.getPixel(j, i);
-      buffer[pixelIndex++] = (image.getRed(pixel) - mean) / std;
-      buffer[pixelIndex++] = (image.getGreen(pixel) - mean) / std;
-      buffer[pixelIndex++] = (image.getBlue(pixel) - mean) / std;
+      buffer[pixelIndex++] = (IMG.getRed(pixel) - mean) / std;
+      buffer[pixelIndex++] = (IMG.getGreen(pixel) - mean) / std;
+      buffer[pixelIndex++] = (IMG.getBlue(pixel) - mean) / std;
     }
   }
   return convertedBytes.buffer.asUint8List();
-}
-
-void TestModel() async {
-  image.Image resizeImage;
-  // image.copyResize(img, width: 64, height: 64);
-  // var out = await Tflite.runModelOnBinary(binary: binary)
-  try {
-    var res;
-    String ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    int score = 0;
-    for (int i = 0; i < 26; i++) {
-      String imgSrc = "assets/images/" + ALPHA[i] + "_test.jpg";
-      // To run model, first use the addAndResize to
-      resizeImage = await addAndResize(imgSrc);
-      res = await Tflite.runModelOnBinary(
-          binary: imageToByteListFloat32(resizeImage, 64, 0.0, 255.0),
-          numResults: 29);
-      if (res[0]['label'] == ALPHA[i]) {
-        score++;
-      }
-    }
-    print("Model Score: " + ((score / 26) * 100).toString() + "%");
-  } catch (e) {
-    print("runModelError: " + e.toString());
-  }
 }
 
 class Home extends StatefulWidget {
@@ -82,7 +45,7 @@ class _HomeState extends State<Home> {
   CameraImage? cameraImage;
   CameraController? cameraController;
   String output = '';
-  Image? image;
+  Image? img;
 
   @override
   initState() {
@@ -101,19 +64,32 @@ class _HomeState extends State<Home> {
       if (!mounted) {
         return;
       } else {
-        setState(() {
-          cameraController!.startImageStream((imageStream) {
-            cameraImage = imageStream;
-            // runModel();
-          });
-        });
+        setState(() {});
       }
     });
   }
 
   Future<void> takePicture() async {
-    XFile file = await cameraController!.takePicture();
-    image = Image.memory(await file.readAsBytes());
+    try {
+      XFile file = await cameraController!.takePicture();
+      Uint8List bytes = await file.readAsBytes();
+      IMG.Image? src = IMG.decodeImage(bytes);
+
+      if (src != null) {
+        IMG.Image destImage = src; //IMG.copyCrop(src, 300, 990, 560, 560);
+
+        IMG.Image resizedImage =
+            IMG.copyResize(destImage, width: 64, height: 64);
+        var res = await Tflite.runModelOnBinary(
+            binary: imageToByteListFloat32(resizedImage, 64, 0.0, 255.0),
+            numResults: 29);
+
+        print("REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESULT");
+        print(res);
+      }
+    } catch (e) {
+      print("runModelError: " + e.toString());
+    }
   }
 
   @override
@@ -132,9 +108,9 @@ class _HomeState extends State<Home> {
               child: !cameraController!.value.isInitialized
                   ? Container()
                   : AspectRatio(
-                aspectRatio: cameraController!.value.aspectRatio,
-                child: CameraPreview(cameraController!),
-              ),
+                      aspectRatio: cameraController!.value.aspectRatio,
+                      child: CameraPreview(cameraController!),
+                    ),
             ),
           ),
           const SizedBox(
